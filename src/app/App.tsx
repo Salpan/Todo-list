2f1bd53a5b1d2bb1ab7dadc50a9e7dd78bd90aaf
Salpan <rus2009ian@gmail.com>
2024-10-30 16:47:03 +0500

    Init prodgect

---
+++ src/app/App.tsx
@@ -0,0 +1,11 @@
+import { MainLayout } from '_components/layouts/main/MainLayout';
+
+function App() {
+    return (
+        <div>
+            <MainLayout />
+        </div>
+    );
+}
+
+export default App;
---
+++ src/common/components/layouts/main/MainLayout.tsx
@@ -0,0 +1,11 @@
+import { FC } from 'react';
+
+export const MainLayout: FC = () => {
+    return (
+        <div>
+            <header>Todo List</header>
+            <main>Тут будет тудушка</main>
+            <footer>Salpan Inc.</footer>
+        </div>
+    );
+};
